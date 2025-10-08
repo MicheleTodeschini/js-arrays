5a8db04e769a16258a21add96ffab3410a9eb476
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-08 13:16:34 +0200

    solved es 3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -16,7 +16,10 @@ const fourthTeacher = null;
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop()
+console.log(teachers);
+console.log(lastTeacher);
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
